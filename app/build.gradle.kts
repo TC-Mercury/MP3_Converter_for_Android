04b31f9f46cdb210c7ab829c1e27df1a25697d71
TC__Mercury <tcmercury1@gmail.com>
2026-05-01 17:51:43 +0300

    style: add button color state list for disabled state

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.mercuryconverter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
 
         ndk {
             abiFilters.add("arm64-v8a")
