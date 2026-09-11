78ae79cad0266b5893ce847b6e1aa368f0e7e6cf
TC__Mercury <tcmercury1@gmail.com>
2026-09-11 16:59:04 +0300

    Changed the versionCode/Name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.mercuryconverter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.3.0"
+        versionCode = 10
+        versionName = "1.3.1"
 
         ndk {
             abiFilters.add("arm64-v8a")
